29d2e75268da98c3f62f9606646bdab9ce179270
denis <strenindenis@gmail.com>
2026-05-19 04:04:08 +0300

    commit_1: create rust project

---
+++ target/debug/deps/subgraph_project_strenin_zaichenko_ISP_231-0935dc7f14b3d94d.d
@@ -0,0 +1,5 @@
+/home/denis/Documents/subgraph_project_strenin_zaichenko_ISP-231/target/debug/deps/subgraph_project_strenin_zaichenko_ISP_231-0935dc7f14b3d94d.d: src/main.rs
+
+/home/denis/Documents/subgraph_project_strenin_zaichenko_ISP-231/target/debug/deps/libsubgraph_project_strenin_zaichenko_ISP_231-0935dc7f14b3d94d.rmeta: src/main.rs
+
+src/main.rs:
