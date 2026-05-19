e2a49378e5277aa344b428f0ba0bc0c0fa15a723
denis <strenindenis@gmail.com>
2026-05-19 04:09:53 +0300

    commit_3: add basic graph data structures

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+// Система поиска частых подграфов
+
+use petgraph::graph::Graph;
+use petgraph::Directed;
+
+/// Метка вершины (например, "Человек", "Фильм")
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct VertexLabel(pub String);
+
+/// Метка ребра (например, "дружит", "смотрел")
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct EdgeLabel(pub String);
+
+/// Тип нашего графа: ориентированный, с метками на вершинах и рёбрах
+type LabeledGraph = Graph<VertexLabel, EdgeLabel, Directed>;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Структуры данных инициализированы");
 }
